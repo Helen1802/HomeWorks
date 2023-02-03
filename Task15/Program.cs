546d53561eb9ccca1a1ddf70504238b313887b34
Elena Sadchikova <79537437744@ya.ru>
2023-02-04 00:17:53 +0300

    задача 15

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+while(n<1||n>7)
+{
+    Console.Write("Вы ошиблись!\nВведите число: ");
+}
+if (n==6||n==7)
+    Console.WriteLine("да");
+else
+    Console.WriteLine("нет");
