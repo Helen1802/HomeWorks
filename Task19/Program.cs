7b990ec9c920083bcb5bab4f7fb9fbeceb732ca4
Elena Sadchikova <79537437744@ya.ru>
2023-02-08 11:47:46 +0300

    решение задачи 19

---
+++ Task19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int n1=n/10000;
+int n2=n/1000%10;
+int n3=n/10%10;
+int n4=n%10;
+if (n1==n4&&n2==n3)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
