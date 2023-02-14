5d94c9806334d125200014b1c48498f51e63bba9
Elena Sadchikova <79537437744@ya.ru>
2023-02-14 23:38:52 +0300

    доп.задача

---
+++ Task5_statistika/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+  array[i] = new Random().Next(1, 32); // [1, 31]
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int countEven = 0, countOdd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] % 2 == 1)
+  {
+    Console.Write($"{array[i]} ");
+    countEven++;
+  }
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+  if (array[i] % 2 == 0)
+  {
+    Console.Write($"{array[i]} ");
+    countOdd++;
+  }
+}
+Console.WriteLine();
+if (countEven <= countOdd)
+  Console.WriteLine("YES");
+else
+  Console.WriteLine("NO");
