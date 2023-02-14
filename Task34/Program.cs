5452fba11588ba9ea4efed594f4f699d8afad44b
Elena Sadchikova <79537437744@ya.ru>
2023-02-14 23:36:29 +0300

    задача 34

---
+++ Task34/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100,1000); // [100; 999]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+int result=0;
+foreach(int element in array)
+{
+    if(element % 2 ==0)
+        result++;
+}
+Console.WriteLine($"Количество четных чисел в массиве {result}");
