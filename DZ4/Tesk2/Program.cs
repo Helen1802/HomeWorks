1a595fa2e5fa6a4e968fcbcacbd42ab3fcdf15cb
Elena Sadchikova <79537437744@ya.ru>
2023-02-14 23:40:08 +0300

    домашнее задание 4

---
+++ DZ4/Tesk2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 1000;
+int n2 = n /100 % 10;
+int n3 = n /10 % 10;
+int n4 = n % 10;
+Console.WriteLine(n1+n2+n3+n4); 
