714d07143a88b710fd507d8c2c9f60f812993940
Elena Sadchikova <79537437744@ya.ru>
2023-02-14 23:37:11 +0300

    задача 36

---
+++ Task36/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] array = new int[4];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100,101); // [-100; 100]
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
+Console.WriteLine(array[1]+array[3]);
