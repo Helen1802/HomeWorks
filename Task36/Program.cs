6005b4b9e5885c4c58a4355f7c7cafe4d9f9e33f
Elena Sadchikova <79537437744@ya.ru>
2023-02-15 18:28:48 +0300

    задача 36

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,6 +1,15 @@
-﻿int[] array = new int[4];
-for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(-100,101); // [-100; 100]
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int i = 0;
+for (i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-100, 101); 
 Console.WriteLine($"[{string.Join(", ", array)}]");
+int sum = 0;
 
-Console.WriteLine(array[1]+array[3]);
+for (int z = 1; z < array.Length; z+=2)
+    sum = sum + array[z];
+
+    Console.WriteLine($"сумма элементов cтоящих на нечётных позициях = {sum}");
+
