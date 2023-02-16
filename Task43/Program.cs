c87691445aa92748eb05fc5ba3befd9a55ff4408
Elena Sadchikova <79537437744@ya.ru>
2023-02-16 12:36:35 +0300

    задача 43

---
+++ Task43/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите коэффициент(1-ой прямой): ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член(1-ой прямой): ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите коэффициент(2-ой прямой): ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите свободный член(2-ой прямой): ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+Console.WriteLine($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+
+
