a0ac3414fa40a655e498535e13771d5b7e5f61c4
Elena Sadchikova <79537437744@ya.ru>
2023-02-16 12:36:07 +0300

    задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите колиство чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()),count = 0;
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+	    count ++;
+}	
+Console.WriteLine($"количество положительных чисел равно: {count}");
