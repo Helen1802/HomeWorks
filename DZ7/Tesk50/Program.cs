6ad7b55e5fc523a3655ae5647cd38c5b582edd57
Elena Sadchikova <79537437744@ya.ru>
2023-02-24 21:38:38 +0300

    DZ 7

---
+++ DZ7/Tesk50/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 21); // [1, 20]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.Write("Введите индекс строки: ");
+int rows= Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца: ");
+int colums= Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[6, 6];// Console.WriteLine("Введите размер массива: "); //int[] matrix = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+InputMatrix(matrix);
+PrintMatrix(matrix);
+if (rows < matrix.GetLength(0) && colums < matrix.GetLength(1)) 
+    Console.WriteLine(matrix[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+
+
---
+++ DZ7/Tesk52/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+int []sum=new int [size[1]];
+
+Random ran = new Random();
+
+for (int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = ran.Next(0, 100);
+
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+
+for (int i=0; i<matrix.GetLength(1); i++)
+{
+    for(int j=0; j<matrix.GetLength(0); j++)
+    {
+        sum[i] += matrix[j, i];
+    }
+}
+
+for (int i=0; i<matrix.GetLength(1); i++)
+{
+    double result=0;
+    result = sum[i] / matrix.GetLength(0);
+    Console.WriteLine($"Среднее арифметическое равно: {result}");
+}
+Console.ReadLine();
